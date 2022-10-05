9e38f69cb1c0aeb5ffa5132f1e4bb9c32af86a38
eoms <96942190+eeoms@users.noreply.github.com>
2022-10-05 10:15:16 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var numJewelsInStones = function(jewels, stones) {
+    var total = 0;
+    
+    for (let i = 0; i < jewels.length; i++) {
+        for (let j = 0; j < stones.length; j++) {
+            if (jewels[i] === stones[j]) {
+                total++
+            }
+        }
+    }
+    return total;
+};
